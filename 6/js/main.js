eb93535328a05919b4a6eefaca40404139964a23
Keks <keks@htmlacademy.ru>
2024-01-28 15:02:22 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,7 @@
+import { drawsThumbnails } from './draws-thumbnails.js';
+import { closeBigPhoto, openBigPhoto } from './open-full-size-photo.js';
+
+drawsThumbnails();
+
+openBigPhoto();
+closeBigPhoto();
