24054400da3c35273f100d1ccafbd9198f1adb15
Наталья Романова <Natalocka_ne@mail.ru>
2024-02-13 09:08:57 +0300

    implemented changing the image scale when clicking on buttons

--- js/upload-file-modal.js
+++ js/upload-file-modal.js
@@ -1,6 +1,12 @@
 import { isEscapeKey } from './utils.js';
 import { onFullSizePhotoEscKeydown } from './open-full-size-photo.js';
 
+const SCALE_STEP = 25;
+const Scale = {
+  MIN: 25,
+  MAX: 100,
+};
+
 const uploadFileModal = document.querySelector('.img-upload__overlay');
 const controlUploadFile = document.querySelector('#upload-file');
 const resetButton = uploadFileModal.querySelector('#upload-cancel');
@@ -8,11 +14,37 @@ const hashtagInput = uploadFileModal.querySelector('.text__hashtags');
 const commentInput = uploadFileModal.querySelector('.text__description');
 const effectsList = uploadFileModal.querySelector('.effects__list');
 const effectPreviewNone = uploadFileModal.querySelector('.effects__preview--none');
+const smallerButton = uploadFileModal.querySelector('.scale__control--smaller');
+const biggerButton = uploadFileModal.querySelector('.scale__control--bigger');
+const scaleValue = uploadFileModal.querySelector('.scale__control--value');
+const imgPreview = uploadFileModal.querySelector('.img-upload__preview img');
 // const effectsPreviewChrome = uploadFileModal.querySelector('.effects__preview--chrome');
 const slider = uploadFileModal.querySelector('.img-upload__effect-level');
 
 // const selectedLabel = uploadFileModal.querySelector(`label[for="${selectedRadioInput.id}"]`);
 // const selectedLabelText = selectedLabel.textContent.trim();
+
+let scale = 100;
+
+const changeScale = (value) => {
+  scale += value;
+  if (scale < Scale.MIN) {
+    scale = Scale.MIN;
+  }
+  if (scale > Scale.MAX) {
+    scale = Scale.MAX;
+  }
+  imgPreview.style.transform = `scale(${scale / 100})`;
+  scaleValue.value = `${scale}%`;
+};
+
+smallerButton.addEventListener('click', () => {
+  changeScale(-`${SCALE_STEP}`);
+});
+
+biggerButton.addEventListener('click', () => {
+  changeScale(+`${SCALE_STEP}`);
+});
 
 effectsList.addEventListener('click', (evt) => {
   const input = evt.target.closest('input');
@@ -29,11 +61,6 @@ effectsList.addEventListener('click', (evt) => {
 const isHashtagInputOnFocus = () => document.activeElement === hashtagInput;
 const isCommentInputOnFocus = () => document.activeElement === commentInput;
 const isEffectPreviewNoneOnFocus = () => document.activeElement === effectPreviewNone;
-
-
-const resizingImage = () => {
-
-};
 
 /**
  * Закрытие формы загрузки фото при нажатии клавиши esc
@@ -63,6 +90,8 @@ const openModal = () => {
   document.removeEventListener('keydown', onFullSizePhotoEscKeydown);
   document.body.classList.add('modal-open');
   uploadFileModal.classList.remove('hidden');
+
+  scaleValue.value = `${scale}%`;
 
   if (isEffectPreviewNoneOnFocus) {
     slider.classList.add('hidden');
